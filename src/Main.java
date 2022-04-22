dd4ed7051740917fded0f9591dde735d1e724054
Niels995 <65388345+Niels995@users.noreply.github.com>
2022-04-22 15:10:01 +0200

    Idk

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Hoofdmenu
+            //-Order selectereno9iio9io
+        //Inpak-robot
+            //- Ziet wat in welke bin zit
+            //- Ziet wat er in gaat zitten
+            //- Ziet of een bin volledig is ingepakt
+        //Orderpick-robot
+            //- Ziet waar de robot is
+            //- Ziet welke route de robot neemt
+            //- Ziet waar welke pruducten staat met voorraad
+    }
+}
