c5d20b78ad900c0f4444e2295047b172024adb22
RoflFlex <61744245+RoflFlex@users.noreply.github.com>
2022-05-23 13:20:59 +0200

    Robot class

---
+++ src/Robots/Robot.java
@@ -0,0 +1,83 @@
+package Robots;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public abstract class Robot {
+    private final String baudRate = "9600";
+    private final String dataBits = "8";
+    private final String stopBits = "1";
+    private final String parityBits = "NO_PARITY";
+    private String dataBuffer;
+    private SerialPort serialPort;
+    private OutputStream outputStream;
+    private String textToPrint;
+
+    public Robot(int serialPort){
+        try{
+            SerialPort portLists[] = SerialPort.getCommPorts();
+            this.serialPort = portLists[serialPort];
+            //System.out.println(comboBoxComport.getSelectedIndex());
+            this.serialPort.setBaudRate(Integer.parseInt(baudRate));
+            this.serialPort.setNumDataBits((Integer.parseInt(dataBits)));
+            this.serialPort.setNumStopBits(Integer.parseInt(stopBits));
+            this.serialPort.setParity(0);
+            this.serialPort.openPort();
+            textToPrint = "Succes";
+        }catch (ArrayIndexOutOfBoundsException e){
+                textToPrint = "Canceled";
+        }
+    }
+
+    public boolean sendInformation(String dataToSend){
+        outputStream = serialPort.getOutputStream();
+        try{
+            outputStream.write(dataToSend.getBytes());
+            return true;
+        }catch (IOException e){
+            return false;
+        }
+    }
+
+    public String getText(int amountCharacters){
+        String textToReturn = "";
+        if(dataBuffer.length() > amountCharacters){
+            textToReturn = dataBuffer.substring(dataBuffer.length()-amountCharacters);
+        }
+        return textToReturn;
+    }
+
+    public String getText(){
+        return getText(10);
+    }
+
+    public String getTextToPrint(){
+        return textToPrint;
+    }
+
+    private void Serial_EventBasedReading(SerialPort activePort){
+        activePort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent serialPortEvent) {
+                System.out.println(serialPortEvent.getEventType());
+                byte []newData = serialPortEvent.getReceivedData();
+                for (int i = 0; i < newData.length; i++){
+                    dataBuffer += (char)newData[i];
+                }
+//                dataBuffer += "\n";
+            }
+        });
+    }
+
+
+
+}
